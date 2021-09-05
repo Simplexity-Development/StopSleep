aec63e743e53e600c058ee562a58eb5a57677d76
IllogicalSong <45906780+illogicalsong@users.noreply.github.com>
2021-09-04 18:56:18 -0700

    Update Stopsleep.java

--- src/main/java/me/adhdmc/stopsleep/Stopsleep.java
+++ src/main/java/me/adhdmc/stopsleep/Stopsleep.java
@@ -1,9 +1,8 @@
 package me.adhdmc.stopsleep;
 
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Stopsleep extends JavaPlugin implements Listener {
+public final class Stopsleep extends JavaPlugin {
 
     @Override
     public void onEnable() {
