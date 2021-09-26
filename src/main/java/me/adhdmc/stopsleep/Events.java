24a2cbbf128eba27d85838274238e1de6665721d
IllogicalSong <45906780+illogicalsong@users.noreply.github.com>
2021-09-26 15:49:23 -0700

    Coreprotect support and more config options

--- src/main/java/me/adhdmc/stopsleep/Events.java
+++ src/main/java/me/adhdmc/stopsleep/Events.java
@@ -1,22 +1,56 @@
 package me.adhdmc.stopsleep;
 
+import net.coreprotect.CoreProtect;
+import net.coreprotect.CoreProtectAPI;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerBedEnterEvent;
+import org.bukkit.plugin.Plugin;
+
+import static org.bukkit.Bukkit.getServer;
+
 
 public class Events implements Listener {
+    private CoreProtectAPI getCoreProtect() {
+        Plugin plugin = getServer().getPluginManager().getPlugin("CoreProtect");
+
+        // Check that CoreProtect is loaded
+        if (plugin == null || !(plugin instanceof CoreProtect)) {
+            return null;
+        }
+
+        // Check that the API is enabled
+        CoreProtectAPI CoreProtect = ((CoreProtect) plugin).getAPI();
+        if (CoreProtect.isEnabled() == false) {
+            return null;
+        }
+
+        // Check that a compatible version of the API is loaded
+        if (CoreProtect.APIVersion() < 7) {
+            return null;
+        }
+
+        return CoreProtect;
+    }
+    CoreProtectAPI api = getCoreProtect();
 
     @EventHandler
     public void onBedInteract(PlayerBedEnterEvent event) {
         Player player = event.getPlayer();
         if (player.hasPermission("stopsleep.bypass")) return;
         event.setCancelled(true);
+        if (Stopsleep.plugin.getConfig().getBoolean("Break Bed", true)) {
+            if (CoreProtect.getInstance().isEnabled()) {
+                api.logRemoval(player.getName(), event.getBed().getLocation(), event.getBed().getType(), null);
+                event.getBed().breakNaturally();
+            }
+        }
         if (Stopsleep.plugin.getConfig().getBoolean("Explosions", true)) {
-            Location location = event.getPlayer().getLocation();
-            player.getWorld().createExplosion(location, 1, false, false);
+            Location location = event.getBed().getLocation();
+            player.getWorld().createExplosion(location, 1, Stopsleep.plugin.getConfig().getBoolean("Explosion Fire", false), Stopsleep.plugin.getConfig().getBoolean("Explosion Block Damage", false));
         }
         if (player.hasPermission("stopsleep.silent")) return;
         if (player.getWorld().getTime() > 12542) {
