3c0d6ea51479589cbddae94d72cd3ce6bc4fa0d4
IllogicalSong <45906780+illogicalsong@users.noreply.github.com>
2021-09-25 19:28:01 -0700

    Add explosions, make message only show up at night

--- src/main/java/me/adhdmc/stopsleep/Events.java
+++ src/main/java/me/adhdmc/stopsleep/Events.java
@@ -1,6 +1,7 @@
 package me.adhdmc.stopsleep;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -9,11 +10,17 @@ import org.bukkit.event.player.PlayerBedEnterEvent;
 public class Events implements Listener {
 
     @EventHandler
-    public void onBedEnter(PlayerBedEnterEvent event){
+    public void onBedInteract(PlayerBedEnterEvent event) {
         Player player = event.getPlayer();
         if (player.hasPermission("stopsleep.bypass")) return;
         event.setCancelled(true);
+        if (Stopsleep.plugin.getConfig().getBoolean("Explosions", true)) {
+            Location location = event.getPlayer().getLocation();
+            player.getWorld().createExplosion(location, 1, false, false);
+        }
         if (player.hasPermission("stopsleep.silent")) return;
-        player.sendMessage(ChatColor.translateAlternateColorCodes('&',Stopsleep.plugin.getConfig().getString("Message", "&cAin't no rest for the wicked")));
+        if (player.getWorld().getTime() > 12542) {
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&', Stopsleep.plugin.getConfig().getString("Message", "&cAin't no rest for the wicked")));
+        }
     }
 }
